625178287f7f5e72c19fdbd4b3f227d2bd66f672
Andras Margittai <thunder6230@gmail.com>
2020-05-09 14:32:39 +0200

    Update app.js

--- app/app.js
+++ app/app.js
@@ -1,56 +1,75 @@
+//Book constructor
 
-const bookForm = document.querySelector('.book-form')
-
-
-
-
-function Book(title, author, isbn) {
+class Book {
+    constructor(title, author, isbn){
     this.title = title
     this.author = author
     this.isbn = isbn
+    }
 
 }
-
-function UI(){
-
-}
-
-        
+//UI Constractor
+class UI {
+    addBookToList(book){
+     const list = document.querySelector('#book-list')
+     const row = document.createElement('tr')
+     row.innerHTML = `
+     
+     <td>${title.value}</td>
+     <td>${author.value}</td>
+     <td>${isbn.value}</td>
+     <td><a href="#" class="delete">X</a></td>
+     
+     `
+     list.appendChild(row)
+    }
+    showAlert(message, className){
+        const div = document.createElement('div')
+        div.className = `alert ${className}`
+        div.appendChild(document.createTextNode(message))
+        const container = document.querySelector('.container')
+        const form = document.querySelector('#book-form')
+        container.insertBefore(div, form)
+        setTimeout(() => {
+            document.querySelector('.alert').remove()
+        }, 3000);
+    }
+    clearAll(){
+        title.value = ''
+        author.value = ''
+        isbn.value = ''
+    }
     
-
-
-UI.prototype.addBookToList = function(book){
-    const list = document.querySelector('#book-list')
-    //create tr element
-    const row = document.createElement('tr')
-    row.innerHTML = `
-    <td>${book.title}</td>
-    <td>${book.author}</td>
-    <td>${book.isbn}</td>
-    <td><a href="#" class="delete">X</a></td>
-    `
-    list.appendChild(row)
-    list.addEventListener('click', function(){
-        if(event.target.classList.contains('delete')){
-            event.target.parentElement.parentElement.remove()
-        }
-    })
+    
+    
 }
 
-bookForm.addEventListener('submit', function(){
+const bookForm = document.querySelector('#book-form')
+const deleteButton = document.querySelector('.delete')
+
+
+bookForm.addEventListener('submit', () => {
     event.preventDefault()
     const title = document.querySelector('#title').value
     const author = document.querySelector('#author').value
     const isbn = document.querySelector('#isbn').value
-    
-    //Instantiate book
     const book = new Book (title, author, isbn)
-    //Instantiate UI
-    const ui = new UI()
-    
-    //add book to list
-    ui.addBookToList(book)
-    
-    
-    
+    //UI
+    const ui = new UI (book)
+    if(title === '' && author === '' && isbn === ''){
+        ui.showAlert('Please add a book!', 'error')
+    }else{
+        ui.addBookToList(book)
+        ui.showAlert('Book has been succesfully added!', 'success')
+        ui.clearAll()
+        document.querySelector('.delete').addEventListener('click', () => {
+            event.target.parentElement.parentElement.remove()
+            ui.showAlert('Book has been deleted!', 'error')
+        })
+        
+    }
 })
+
+
+
+
